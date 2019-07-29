194f4357e0d0a0754af4155f9bf1d2bbd2ef86ee
akram <akram.berkawy@gmail.com>
2019-07-29 20:45:01 +0300

    - added travis configs - remove Chrome path from tests

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Shotify
-Html to PDF and web page screenshots using headless Chrome with no need for Nodejs/npm.
+# Shotify: Html to PDF and web page screenshots using headless Chrome 
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Build Status](https://travis-ci.com/AkramBerkawy/shotify.svg?branch=master)](https://travis-ci.com/AkramBerkawy/shotify)
+![Packagist Version](https://img.shields.io/packagist/v/akrambe/shotify)
+![PHP from Packagist](https://img.shields.io/packagist/php-v/akrambe/shotify)
+
+No need for Nodejs/npm!
 
 # Documentation
 * [Installation](#installation)
@@ -21,21 +26,18 @@ require('vendor/autoload.php');
 
 // PDF print from url
 Shotify\Launcher::fromUrl('http://example.com')
-            ->setChromeExePath('/path/to/chrome')
             ->saveToPDF('example.pdf');
 
 // Screenshot
 Shotify\Launcher::fromUrl('http://example.com')
-            ->setChromeExePath('/path/to/chrome')
             ->captureScreenshot('hello.png',
-                (new ScreenshotOptions)
+                (new Shotify\Options\ScreenshotOptions)
                     ->setQuality(70)
                     ->setFormat('png')
             );
             
 // Get document outer HTML
 $html = Shotify\Launcher::fromUrl('http://example.com')
-            ->setChromeExePath('/path/to/chrome')
             ->outerHtml();
             
 ```
--- tests/LauncherTest.php
+++ tests/LauncherTest.php
@@ -12,12 +12,10 @@ class LauncherTest extends TestCase
     var $testHtmlTitleAssert = '<title>Welcome HTML</title>';
     var $testUrl = 'http://example.com/';
     var $testUrlTitleAssert = '<title>Example Domain</title>';
-    var $exePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe';
 
     public function test_from_url()
     {
         $html = Launcher::fromUrl($this->testUrl)
-            ->setChromeExePath($this->exePath)
             ->outerHtml();
 
         self::assertStringContainsString($this->testUrlTitleAssert, $html);
@@ -26,7 +24,6 @@ class LauncherTest extends TestCase
     public function test_with_html()
     {
         $html = Launcher::withHtml($this->testHtml)
-            ->setChromeExePath($this->exePath)
             ->outerHtml();
 
         self::assertStringContainsString($this->testHtmlTitleAssert, $html);
@@ -35,7 +32,6 @@ class LauncherTest extends TestCase
     public function test_outer_html()
     {
         $html = Launcher::withHtml($this->testHtml)
-            ->setChromeExePath($this->exePath)
             ->outerHtml();
 
         self::assertStringContainsString($this->testHtml, $html);
@@ -44,7 +40,6 @@ class LauncherTest extends TestCase
     public function test_print_to_pdf_from_url()
     {
         $result = Launcher::fromUrl($this->testUrl)
-            ->setChromeExePath($this->exePath)
             ->saveToPDF('hello.pdf');
 
         self::assertTrue($result);
@@ -54,7 +49,6 @@ class LauncherTest extends TestCase
     public function test_screenshot()
     {
         $result = Launcher::fromUrl($this->testUrl)
-            ->setChromeExePath($this->exePath)
             ->captureScreenshot('hello.jpg');
         self::assertTrue($result);
         self::assertFileExists('hello.jpg');
@@ -63,7 +57,6 @@ class LauncherTest extends TestCase
     public function test_screenshot_with_options()
     {
         $result = Launcher::fromUrl($this->testUrl)
-            ->setChromeExePath($this->exePath)
             ->captureScreenshot('hello.png',
                 (new ScreenshotOptions)
                     ->setQuality(70)
